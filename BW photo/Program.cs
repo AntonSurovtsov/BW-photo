1749baab61544d0f40048ed2c1439365086b65c7
Антон Суровцов <asurovcov@outlook.com>
2022-04-04 21:11:20 +0300

    Добавьте файлы проекта.

---
+++ BW photo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BW_photo
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.WriteLine("Press enter to start...");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+
+                bitmap.ToGrayscale();
+
+                bitmap.Save($"{openFileDialog.FileName.Substring(0, openFileDialog.FileName.Length - 4)}BW.jpg");
+
+                Console.WriteLine("Complete!");
+            }
+         }
+        
+    }
+}
